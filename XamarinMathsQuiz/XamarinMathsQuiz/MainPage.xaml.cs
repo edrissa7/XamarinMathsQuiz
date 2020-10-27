967b5ec2230dc7cd406af46d14b2a6f2ca3d2f34
unknown <ed.sarr7@gmail.com>
2020-10-27 19:36:43 -0100

    first c

---
+++ XamarinMathsQuiz/XamarinMathsQuiz/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinMathsQuiz
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        GameManager gameManager;
+        public MainPage()
+        {
+            InitializeComponent();
+             gameManager = new GameManager();
+            correctLabel.Text = "Correct = " + gameManager.CorrectCount;
+            wrongLabel.Text = "Wrong = " + gameManager.WrongCount;
+            updateView();
+              
+          
+        }
+
+        private void But_Clicked(object sender, EventArgs e)
+        {
+         
+            Button btn = (Button)sender;
+            switch (btn.StyleId) {
+                case "1":
+                    gameManager.checkAnswer(int.Parse(But1.Text));
+                    gameManager.generateQuestion();
+                    updateView();
+                    break;
+                case "2":
+                    gameManager.checkAnswer(int.Parse(But2.Text));
+                    gameManager.generateQuestion();
+                    updateView();
+                    break;
+                case "3":
+                    gameManager.checkAnswer(int.Parse(But3.Text));
+                    gameManager.generateQuestion();
+                    updateView();
+                    break;
+                case "4":
+                    gameManager.checkAnswer(int.Parse(But4.Text));
+                    gameManager.generateQuestion();
+                    updateView();
+                    break;
+            }
+          
+        }
+
+           private void updateView() {
+            
+
+            But1.Text = gameManager.getCurrentQuestion().getAnswerArray()[0].ToString();
+            But2.Text = gameManager.getCurrentQuestion().getAnswerArray()[1].ToString();
+            But3.Text = gameManager.getCurrentQuestion().getAnswerArray()[2].ToString();
+            But4.Text = gameManager.getCurrentQuestion().getAnswerArray()[3].ToString();
+
+            questionText.Text = gameManager.getCurrentQuestion().getQuestionPhrase();
+
+            correctLabel.Text = "Correct = " + gameManager.CorrectCount;
+            wrongLabel.Text = "Wrong = " + gameManager.WrongCount;
+        }
+    }
+}
---
+++ XamarinMathsQuiz/XamarinMathsQuiz/Question.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinMathsQuiz
+{
+    class Question
+    {
+        private int firstNumber;
+        private int secondNumber;
+        private int answer;
+        private int[] answerArray = new int[4];
+        private int answerPosition;
+        //private int upperLimit;
+        private string questionPhrase;
+
+        public Question(int upperLimit) {
+            firstNumber = new Random().Next(upperLimit);
+            secondNumber = new Random().Next(upperLimit);
+            answer = firstNumber + secondNumber;
+            questionPhrase = firstNumber + " + " + secondNumber + " = ";
+            answerPosition = new Random().Next(4);
+            
+
+                this.answerArray[0] = answer + 1;
+                this.answerArray[1] = answer + 4;
+                this.answerArray[2] = answer + 7;
+                this.answerArray[3] = answer + 2;
+
+            ShuffleArray(this.answerArray);
+
+                this.answerArray[answerPosition] = answer;           
+                  
+
+        }
+
+        public int[] getAnswerArray() {
+            return this.answerArray;
+        }
+
+        public void setQuestionPhrase(string questionPhrase) {
+            this.questionPhrase = questionPhrase;
+        }
+
+        public string getQuestionPhrase() {
+            return questionPhrase;
+        }
+
+        public int getAnswer() {
+            return this.answer;
+        }
+
+        public void ShuffleArray(int[] array) {
+            int randomIndex = new Random().Next(4);
+            for (int i = 0; i < array.Length; i++) {
+                int temp = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = temp;
+            }
+            
+        }
+    }
+}
